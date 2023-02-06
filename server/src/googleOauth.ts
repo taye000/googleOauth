d68894dc97ff6954d50c2805aa88317dba5cfd82
taye000 <taylorgitari@gmail.com>
2023-02-06 22:15:39 +0300

    jest

--- server/src/googleOauth.ts
+++ server/src/googleOauth.ts
@@ -14,7 +14,7 @@ passport.deserializeUser((user: any, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientID: process.env..GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL: "http://localhost:3000/auth/google/callback",
       passReqToCallback: true,
