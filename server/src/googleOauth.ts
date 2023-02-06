c98bd19b8f060e1503bcc6c911bf1e6b4f65c270
taye000 <taylorgitari@gmail.com>
2023-02-06 21:39:08 +0300

    google oauth

---
+++ server/src/googleOauth.ts
@@ -0,0 +1,44 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+//serialize user
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+//deserialize user
+passport.deserializeUser((user: any, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      console.log(profile, "profile");
+
+      //check if the user is registered
+      user.findOne(
+        { googleId: profile.id },
+        async function (err: Error, user: any) {
+          if (!user) {
+            //if user is not in the system, create new user
+            user = new user({ googleId: profile.id }, async function (
+              err: Error,
+              user: any
+            ) {
+              //save new user to db
+              await user.save();
+              return done(err, user);
+            });
+          }
+        }
+      );
+    }
+  )
+);
